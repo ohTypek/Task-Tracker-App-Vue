f3296b000310406b764ea68d1c4342f8057b74d9
ohTypek <toptypek@wp.pl>
2022-09-18 15:30:26 +0200

    Inital Commit

---
+++ src/components/HeaderC.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'red' : 'green' "/>
+  </header>
+</template>
+
+<script>
+import ButtonC from './ButtonC.vue';
+
+export default {
+  name: 'HeaderPart',
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+  components: {
+    Button: ButtonC
+  },
+  emits: ['toggle-add-task']
+}
+</script>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
---
+++ src/components/TasksC.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <div v-for="task in tasks" :key="task.id" >
+      <Task @toggle-reminder="$emit('toggle-reminder', task.id)" :task="task" @delete-task="$emit('delete-task', task.id)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TaskC from './TaskC.vue'
+
+export default {
+  name: 'TaskC',
+  props: {
+    tasks: []
+  },
+  components: {
+    Task: TaskC
+  },
+  emits: ['delete-task', 'toggle-reminder']
+}
+</script>
